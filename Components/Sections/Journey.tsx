bc1bbdbf1fd64b9e5421356cfdeb0cc5352fa0aa
BalaAyyappan1 <balaayyappan11@gmail.com>
2025-06-09 11:49:43 +0530

    chanegs

--- Components/Sections/Journey.tsx
+++ Components/Sections/Journey.tsx
@@ -294,7 +294,7 @@ const Journey = () => {
       </div>
 
       {/* The beginning */}
-      <div className="absolute 2xl:top-94 2xl:left-55 xl:top-81 xl:left-45 lg:top-70 lg:left-45 md:top-83 md:left-35 w-full flex flex-row justify-center items-center gap-8 px-4">
+      <div className="absolute 3xl:top-94 3xl:left-55 xl:top-81 xl:left-45 lg:top-70 lg:left-45 md:top-83 md:left-35 w-full flex flex-row justify-center items-center gap-8 px-4">
         <span className="text-[#FFFDFA] xl:text-[24px] lg:text-[20px] font-semibold text-right">
           The Beginning
         </span>
@@ -315,7 +315,7 @@ const Journey = () => {
       </div>
 
       {/* What's Ahead */}
-      <div className="absolute 2xl:top-173 2xl:right-55 xl:top-151 xl:right-40 lg:top-131 lg:right-40 md:top-155 md:right-30 w-full flex flex-row justify-center items-center gap-8 px-4">
+      <div className="absolute 3xl:top-173 3xl:right-55 xl:top-152 xl:right-40 lg:top-131 lg:right-40 md:top-155 md:right-30 w-full flex flex-row justify-center items-center gap-8 px-4">
         <span className="text-[#BEBCBA] xl:text-[20px] lg:text-[18px] w-1/3 text-left">
           From an idea to a growing ecosystem, our goal is to launch ventures
           that are bold, practical, and globally scalable — starting right here
@@ -336,7 +336,7 @@ const Journey = () => {
       </div>
 
       {/* Milestone One */}
-      <div className="absolute 2xl:top-268 2xl:right-60 xl:top-235 lg:top-26 xl:left-35 lg:left-25 md:top-241 md:left-35 w-full flex flex-row justify-center items-center gap-8 px-4">
+      <div className="absolute 3xl:top-268 3xl:right-60 xl:top-235 lg:top-26 xl:left-35 lg:left-25 md:top-241 md:left-35 w-full flex flex-row justify-center items-center gap-8 px-4">
         <span className="text-[#FFFDFA] xl:text-[24px] lg:text-[20px] w-1/4 text-right">
           Milestone One
         </span>
@@ -357,7 +357,7 @@ const Journey = () => {
       </div>
 
       {/* Q3 2025 */}
-      <div className="absolute 2xl:bottom-47 2xl:left-38 xl:bottom-36 xl:left-30 lg:bottom-28 lg:left-28 md:bottom-37 md:left-25 w-full flex flex-row justify-center items-center gap-8 px-4">
+      <div className="absolute 3xl:bottom-47 3xl:left-38 xl:bottom-36 xl:left-30 lg:bottom-28 lg:left-28 md:bottom-37 md:left-25 w-full flex flex-row justify-center items-center gap-8 px-4">
         <span className="text-white xl:text-[24px] lg:text-[20px] w-1/4 text-right">
           Q3 2025
         </span>
@@ -369,7 +369,7 @@ const Journey = () => {
       </div>
 
       {/* 2026 */}
-      <div className="absolute xl:bottom-13 xl:left-7 lg:bottom-6 lg:left-8 md:bottom-12 w-full flex flex-row justify-center items-center gap-8 px-4">
+      <div className="absolute xl:bottom-10 xl:left-7 lg:bottom-6 lg:left-8 md:bottom-12 w-full flex flex-row justify-center items-center gap-8 px-4">
         <span className="text-white xl:text-[24px] lg:text-[20px] w-1/4 text-right">
           2026
         </span>
