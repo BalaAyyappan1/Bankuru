39dd198a145a1d120b3e6e9e99653a77c87e4a70
BalaAyyappan1 <balaayyappan11@gmail.com>
2025-06-16 15:32:55 +0530

    building optimised

--- Components/Sections/Mission.tsx
+++ Components/Sections/Mission.tsx
@@ -328,7 +328,7 @@ const Mission: React.FC = () => {
                   backfaceVisibility: 'hidden'
                 }}
                 priority
-                unoptimized
+                unoptimized={true}
               />
             </div>
           )}
